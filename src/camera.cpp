517882707dc0aa8f555f29fbeba7447694ceec7a
MusaTamzid05 <tamzid101@gmail.com>
2023-12-02 00:36:14 +0600

    Camera speed increase, zfar and znear limit fix

--- src/camera.cpp
+++ src/camera.cpp
@@ -13,7 +13,7 @@ Camera::Camera() {
     m_yaw = -90.0f;
     m_pitch = 0.0f;
     zoom = 45.0f;
-    m_speed = 2.5f;
+    m_speed = 20.0f;
     m_sensitivity = 0.1f;
 
 }
@@ -48,8 +48,8 @@ glm::mat4 Camera::get_projection() const {
     return glm::perspective(
             glm::radians(zoom),
             (float) WIDTH / (float) HEIGHT,
-            0.01f,
-            10.0f
+            0.1f,
+            100.0f
             );
 
 }
